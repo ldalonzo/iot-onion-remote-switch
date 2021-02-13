eafe30d0b1cbf77d0e22a1aa5e74cd7e4154f51d
Leonardo D'Alonzo <725483+ldalonzo@users.noreply.github.com>
2021-02-13 20:09:08 +0000

    Build and use Azure IoT SDK

--- main.cpp
+++ main.cpp
@@ -3,9 +3,17 @@
 
 #include "OnionConfig.h"
 
+#include "iothub.h"
+
 int main(int argc, char *argv[])
 {
   std::cout << argv[0] << " Version " << Onion_VERSION_MAJOR << "." << Onion_VERSION_MINOR << std::endl;
+
+  if (IoTHub_Init() != 0)
+  {
+    std::cout << "Failed to initialize the platform." << std::endl;
+    return -1;
+  }
 
   return 0;
 }
